c15a7c24f00a58b7d6a06d0cbe333f1aff3954d6
Roman <roman.vysotsky@fh-bingen.de>
2017-09-18 18:42:24 +0200

    Added another screenshot for explanation.

--- app/src/main/java/com/example/roman/audiocuttertest/intro/IntroActivity.java
+++ app/src/main/java/com/example/roman/audiocuttertest/intro/IntroActivity.java
@@ -20,7 +20,7 @@ public class IntroActivity extends AppIntro {
         super.onCreate(savedInstanceState);
 
         addSlide(AppIntroFragment.newInstance(getString(R.string.intro_slide1_title), getString(R.string.intro_slide1_text), R.mipmap.ic_launcher_sci, ContextCompat.getColor(this, R.color.colorPrimary)));
-        addSlide(AppIntroFragment.newInstance(getString(R.string.intro_slide2_title), getString(R.string.intro_slide2_text), R.drawable.ic_share_white_128dp, ContextCompat.getColor(this, R.color.colorPrimary)));
+        addSlide(AppIntroFragment.newInstance(getString(R.string.intro_slide2_title), getString(R.string.intro_slide2_text), R.drawable.share_slide, ContextCompat.getColor(this, R.color.colorPrimary)));
         addSlide(AppIntroFragment.newInstance(getString(R.string.intro_slide3_title), getString(R.string.intro_slide3_text), R.drawable.cut_slide, ContextCompat.getColor(this, R.color.colorPrimaryDark)));
         addSlide(AppIntroFragment.newInstance(getString(R.string.intro_slide4_title), getString(R.string.intro_slide4_text), R.drawable.directory_slide, ContextCompat.getColor(this, R.color.colorPrimaryDark)));
         addSlide(AppIntroFragment.newInstance(getString(R.string.intro_slide5_title), getString(R.string.intro_slide5_text), R.drawable.combine_slide, ContextCompat.getColor(this, R.color.colorPrimaryDark)));
